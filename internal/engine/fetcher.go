fb4f6bbc288e5da87b343182cf303a24374ac8d9
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-11-23 14:24:23 -0500

    chore: remove unused getPrice function

--- internal/engine/fetcher.go
+++ internal/engine/fetcher.go
@@ -1,20 +1,11 @@
 package engine
 
 import (
-	"math/rand"
 	"sync"
 	"time"
 
 	"github.com/koralkulacoglu/smart-order-router/internal/models"
 )
-
-func getPrice() float64 {
-	basePrice := 98000.00
-	spread := 0.2
-	price := basePrice * (1 + rand.Float64()*spread*2 - spread)
-
-	return price
-}
 
 func FetchQuote(id int, exchange models.Exchange, results chan<- models.Quote, wg *sync.WaitGroup) {
 	defer wg.Done()
